beea3cd773b78b794f92c716a5ff9d577416b93c
kdmukai <kdmukai@gmail.com>
2022-12-30 12:18:54 -0600

    multithreading support

--- README.md
+++ README.md
@@ -48,6 +48,8 @@ positional arguments:
 optional arguments:
   -h, --help         show this help message and exit
   -e, --include-end  Also search the end of the npub
+  -j NUM_JOBS, --jobs NUM_JOBS
+                        Number of threads (default: 4)
 ```
 
 ## Limitations
@@ -79,7 +81,7 @@ Also note that you can stop a vanity `npub` search and restart it later. You're 
 
 
 ## Performance
-On an M1 Macbook Pro:
+Single-threaded on an M1 Macbook Pro:
 ```
 1876.9s: Tried 18,000,000 npubs so far
 1981.2s: Tried 19,000,000 npubs so far
@@ -87,7 +89,19 @@ On an M1 Macbook Pro:
 2189.9s: Tried 21,000,000 npubs so far
 ```
 
-Expect a 5-char vanity target to take a couple million tries.
+A 5-char vanity target could easily take tens of millions of tries.
+
+Run with a `-j` setting that matches your CPU's threading abilities:
+```
+# Brute force w/eight threads
+python3 vanity_npub.py -j 8 n0str
+
+# Maximize threads on linux:
+python3 vanity_npub.py -j "$(($(nproc)+1))" n0str
+
+# Maximize threads on macOS:
+python3 vanity_npub.py -j "$(($(sysctl -n hw.physicalcpu)+1))" n0str
+```
 
 
 ## Is this secure?
--- vanity_npub.py
+++ vanity_npub.py
@@ -3,32 +3,73 @@
 
     https://github.com/kdmukai/nostr_vanity_npub
 """
-import time
+from threading import Event, Lock, Thread
 from nostr.key import PrivateKey
 
 
-def find_vanity_npub(target: str, include_end: bool = False):
-    start = time.time()
-    i = 0
-    while True:
-        i += 1
-        pk = PrivateKey()
-        npub = pk.public_key.bech32()[5:]   # Trim the "npub1" prefix
 
-        if npub[:len(target)] != target and (include_end is False or (include_end and npub[-1*len(target):] != target)):
-            if i % 1e6 == 0:
-                print(f"{(time.time() - start):0.1f}s: Tried {i:,} npubs so far")
-            continue
-        
-        print(f"\n{i:,} | {(time.time() - start):0.1f}s | npub1{npub}")
-        break
-    return pk
+class ThreadsafeCounter:
+    """
+        Copied from SeedSigner
+    """
+    def __init__(self, initial_value: int = 0):
+        self.count = initial_value
+        self._lock = Lock()
+    
+    @property
+    def cur_count(self):
+        # Reads don't require the lock
+        return self.count
+
+    def increment(self, step: int = 1):
+        # Updates must be locked
+        with self._lock:
+            self.count += step
+    
+    def set_value(self, value: int):
+        with self._lock:
+            self.count = value
+
+
+
+class BruteForceThread(Thread):
+    def __init__(self, target: str, threadsafe_counter: ThreadsafeCounter, event: Event, include_end: bool = False):
+        super().__init__(daemon=True)
+        self.target = target
+        self.threadsafe_counter = threadsafe_counter
+        self.event = event
+        self.include_end = include_end
+        self.pk = None
+
+
+    def run(self):
+        i = 0
+        while not self.event.is_set():
+            i += 1
+            pk = PrivateKey()
+            npub = pk.public_key.bech32()[5:]   # Trim the "npub1" prefix
+
+            if npub[:len(target)] != target and (include_end is False or (include_end and npub[-1*len(target):] != target)):
+                increment_check = 1e5
+                if i % increment_check == 0:
+                    self.threadsafe_counter.increment(increment_check)
+                    print(f"Tried {int(self.threadsafe_counter.cur_count):,} npubs so far", flush=True)
+                continue
+            
+            print(f"\n{i:,} | {(time.time() - start):0.1f}s | npub1{npub}")
+            self.pk = pk
+            self.event.set()
+            break
 
 
 
 if __name__ == "__main__":
     import argparse
+    import time
+    from threading import Event
     from nostr import bech32
+
+
     parser = argparse.ArgumentParser(
         description="********************** Nostr vanity pubkey generator **********************\n\n" + \
             "Search for `target` in an npub such that:\n\n" + \
@@ -40,16 +81,23 @@ if __name__ == "__main__":
     parser.add_argument('target', type=str,
                         help="The string you're looking for")
 
-    # Optional switches
+    # Optional arguments
     parser.add_argument('-e', '--include-end',
                         action="store_true",
                         default=False,
                         dest="include_end",
                         help="Also search the end of the npub")
 
+    parser.add_argument('-j', '--jobs',
+                    default=4,
+                    type=int,
+                    dest="num_jobs",
+                    help="Number of threads (default: 4)")
+
     args = parser.parse_args()
     target = args.target.lower()
     include_end = args.include_end
+    num_jobs = args.num_jobs
 
     for i in range(0, len(target)):
         if target[i] not in bech32.CHARSET:
@@ -57,7 +105,7 @@ if __name__ == "__main__":
             print(f"""\tbech32 chars: {"".join(sorted(bech32.CHARSET))}""")
             exit()
     
-    if len(target) > 6:
+    if len(target) >= 6:
         # 
         print(
             "This will probably take a LONG time!\n\n" + \
@@ -65,5 +113,23 @@ if __name__ == "__main__":
             "Working..."
         )
 
-    pk = find_vanity_npub(target, include_end=include_end)
-    print(f"""\n\t{"*"*76}\n\tPrivate key: {pk.bech32()}\n\t{"*"*76}\n""")
+    start = time.time()
+    threadsafe_counter = ThreadsafeCounter()
+    event = Event()
+
+    threads = []
+    for i in range(0, num_jobs):
+        brute_force_thread = BruteForceThread(target, threadsafe_counter=threadsafe_counter, event=event, include_end=include_end)
+        brute_force_thread.start()
+        threads.append(brute_force_thread)
+    
+    print(f"Initialized {num_jobs} threads")
+
+    # Block until the first thread exits; after one thread finds a match, it will set the
+    #   Event and all threads will exit.
+    threads[0].join()            
+
+    for thread in threads:
+        if threads[i].pk is not None:
+            print(f"""\n\t{"*"*76}\n\tPrivate key: {threads[i].pk.bech32()}\n\t{"*"*76}\n""")
+            exit(0)
